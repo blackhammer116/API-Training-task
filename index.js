d0ad815e0fbea4689c12ff9c562a27694910b55b
CodersKin <nebiyuhiyabu@gmail.com>
2024-12-24 10:45:05 +0300

    minor changes on both files

--- index.js
+++ index.js
@@ -7,18 +7,10 @@ const router = express.Router();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const courses = [
-    {id: 1, name: 'course1'},
-    {id: 2, name: 'course2'},
-    {id: 3, name: 'course3'},
-]
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+'/index.html'));
 });
-// router.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname+'./index.html'));
-// });
 
 app.post('/validate', (req, res) => {
     const { error , value} = validateCredentials(req.body);
@@ -26,12 +18,6 @@ app.post('/validate', (req, res) => {
         res.status(400).send(error.details[0].message);
     }
     res.send("<html><script> alert('Validation successful!!!')</script></html>");
-});
-
-app.get('/api/courses/:id', (req, res) => {
-    const course = courses.find(c => c.id === parseInt(req.params.id));
-    if (!course) res.status(404).send("the course with the given id not found");
-    res.send(course);
 });
 
 function validateCredentials(credentials){
